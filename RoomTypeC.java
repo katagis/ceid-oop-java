3e16fcaa8cc7face5638bd4457202cec324a667b
harrycoderk <fox.dealos@gmail.com>
2017-04-23 19:40:53 +0300

    Add RoomTypeC

---
+++ RoomTypeC.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Harry on 4/23/2017.
+ */
+public class RoomTypeC extends Room {
+    int MinPeople;
+    int MinDays;
+
+    RoomTypeC(int maxPeople, double pricePerPerson, int minPeople, int minDays) {
+        super(maxPeople, pricePerPerson);
+        MinPeople = minPeople;
+        MinDays = minDays;
+    }
+
+    @Override
+    boolean AddReservation(Reservation reservation) {
+        if (reservation.Days < MinDays) {
+            return false;
+        }
+        if (reservation.NumberOfPeople < MinPeople) {
+            return false;
+        }
+        return super.AddReservation(reservation);
+    }
+}
